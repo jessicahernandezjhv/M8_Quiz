be47bfe2fdf8868af6ada1746d9e20b8e9cb2dcc
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2018-11-07 19:50:23 +0100

    Variables changed

---
+++ app/src/main/java/com/m8/jessica_hernandez/quizy/MainQuiz.java
@@ -0,0 +1,93 @@
+package com.m8.jessica_hernandez.quizy;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+public class MainQuiz extends AppCompatActivity
+        implements QuestionsFragment.QuestionsFragmentListener{
+
+    private int answers = 0;
+    private boolean gameFinished = false;
+
+    private SharedPreferences.Editor prefEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        sharedPreferences();
+
+        QuestionsFragment questionsFragment = QuestionsFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, questionsFragment)
+                .commit();
+    }
+
+    private void sharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences(
+                getString(R.string.my_preferences), Context.MODE_PRIVATE);
+
+        answers = preferences.getInt(getString(R.string.saved_answeres), 0);
+
+        gameFinished = preferences.getBoolean(getString(R.string.saved_game_state), false);
+
+        QuestionList.setIndex(
+                preferences.getInt(getString(R.string.saved_next_question), 0));
+
+        prefEditor = preferences.edit();
+    }
+
+    private void saveSharedPreferences() {
+        prefEditor.putInt(getString(R.string.saved_answeres), answers);
+        prefEditor.putBoolean(getString(R.string.saved_game_state), gameFinished);
+        prefEditor.putInt(getString(R.string.saved_next_question), QuestionList.getIndex());
+        prefEditor.commit();
+    }
+
+    public String compareAnswere(boolean answerGiven){
+        String res = "Game Finished (" + answers + " answers of " +
+                QuestionList.getNumQuestions() + " questions).";
+
+        if (!gameFinished) {
+
+            String toastMessage = getString(R.string.toast_incorrect);
+            if (QuestionList.checkAnswere(answerGiven)) {
+                toastMessage = getString(R.string.toast_correct);
+                answers++;
+            }
+
+            Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+
+            gameFinished = !QuestionList.next();
+
+            if (!gameFinished)
+                res = QuestionList.getCurrentQuestionText();
+        }
+
+        saveSharedPreferences();
+        return res;
+    }
+
+    @Override
+    public void restartGame() {
+        answers = 0;
+        gameFinished = false;
+        saveSharedPreferences();
+    }
+
+    @Override
+    public void startHint() {
+        if (!gameFinished) {
+            DialogFragment hint = new HintDialog();
+
+            hint.show(getSupportFragmentManager(), "Hint");
+        }
+    }
+}
